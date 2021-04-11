c3bf900919d87801ba9179bd52a741221285e2d0
Ethan <ethanblake@gmail.com>
2021-04-11 00:02:39 -0600

    Remove dart_eval and flutter_eval

--- dart_eval/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## 0.0.1
-
-- Initial version, created by Stagehand
-
-## 0.0.2
-
-- Added basic interpreter
--- dart_eval/lib/src/parse/source.dart
+++
@@ -1,7 +0,0 @@
-/// Represents a node in Dart source code
-abstract class DartSourceNode {
-  final int offset;
-  final int length;
-
-  const DartSourceNode(this.offset, this.length);
-}
